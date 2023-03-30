5965e311c6713879a3310551617ebacef10cc14a
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-03-31 01:36:49 +0530

    Create Ques_1431 Kids With the Greatest Number of Candies.java

---
+++ Arrays_--/Ques_1431 Kids With the Greatest Number of Candies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] arr, int ext) {
+        ArrayList<Boolean> lst=new ArrayList<>();
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            max=Math.max(arr[i],max);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]+ext>=max){
+                lst.add(true);
+            }
+            else{
+                lst.add(false);
+            }
+        }
+        return lst;
+    }
+}
