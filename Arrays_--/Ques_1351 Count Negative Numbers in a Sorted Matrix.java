974b335339771c91bbb996cee00efdf21656cece
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-04-02 01:46:00 +0530

    Create Ques_1351 Count Negative Numbers in a Sorted Matrix.java

---
+++ Arrays_--/Ques_1351 Count Negative Numbers in a Sorted Matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int cnt=0;
+        for(int i=0;i<grid.length;i++){
+          cnt+=Neg_cnt(grid[i]);
+        }
+        return cnt;
+    }
+    public int Neg_cnt(int arr[]){
+      int spt=0;
+      int ept=arr.length;
+      while(spt<ept){
+        int mid=spt+(ept-spt)/2;
+        if(arr[mid]<0)ept=mid;
+        else spt=mid+1;
+      }
+      return arr.length-spt;
+    }
+}
