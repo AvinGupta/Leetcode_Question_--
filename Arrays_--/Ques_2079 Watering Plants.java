f39b87de79886b08d980d26b2406d526e118493a
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-04-01 20:55:45 +0530

    Create Ques_2079 Watering Plants.java

---
+++ Arrays_--/Ques_2079 Watering Plants.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int wateringPlants(int[] arr, int cap) {
+        int cnt=0;
+        int var=cap;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=cap){
+                cnt++;
+                cap-=arr[i];
+            }
+            else{
+                cap=var;
+                cnt+=i;
+                cnt+=(i+1);
+                cap-=arr[i];
+            }
+        }
+        return cnt;
+    }
+}
