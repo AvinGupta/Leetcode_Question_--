717216dbad342fee32fcc481087a87fd528566fd
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-04-06 16:25:49 +0530

    Create Ques_2535 Difference Between Element Sum and Digit Sum of an Array.java

---
+++ Arrays_--/Ques_2535 Difference Between Element Sum and Digit Sum of an Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int ELE_sum=0;
+        int DIG_sum=0;
+        for(int var:nums){
+            ELE_sum+=var;
+            DIG_sum+=dig(var);
+        }
+        return Math.abs(DIG_sum-ELE_sum);
+    }
+    public static int dig(int num){
+        int sum=0;
+        while(num!=0){
+            sum+=num%10;
+            num/=10;
+        }
+        return sum;
+    }
+}
