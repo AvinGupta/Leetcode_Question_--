f95a74f1cc1c2ba18ca01ae0cd04d5bb0c256c45
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-04-15 21:31:38 +0530

    Update Ques_1823 Find the Winner of the circular game.java

--- Recursion_and_Backtracking_--/Ques_1823 Find the Winner of the circular game.java
+++ Recursion_and_Backtracking_--/Ques_1823 Find the Winner of the circular game.java
@@ -5,6 +5,6 @@ class Solution {
     static int Josephus_prob(int n,int k){
         if(n==1) return 0;
         return (Josephus_prob(n-1,k)+k)%n;//here we are adding k because for every call starting index is changing 
-                                          //here we are taking %n so that answer+k should not exceed n;
+                                          //here we are taking %n so that answer+k should not exceed n;  
     }
 }
