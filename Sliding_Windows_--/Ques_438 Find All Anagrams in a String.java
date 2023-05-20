10f3875b835e4cee6b297f389a1611911487a00a
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-05-20 23:40:58 +0530

    Create Ques_438 Find All Anagrams in a String.java

---
+++ Sliding_Windows_--/Ques_438 Find All Anagrams in a String.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> lst=new ArrayList<>();
+        int k=p.length();
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<p.length();i++){
+            if(!map.containsKey(p.charAt(i))) map.put(p.charAt(i),1);
+            else map.put(p.charAt(i),map.get(p.charAt(i))+1);
+        }
+        int s_pt=0;
+        int e_pt=0;
+        int cnt=map.size();
+        while(e_pt<s.length()){
+            if(map.containsKey(s.charAt(e_pt))){
+                map.put(s.charAt(e_pt),map.get(s.charAt(e_pt))-1);
+                if(map.get(s.charAt(e_pt))==0) cnt--;
+            }
+            if(e_pt-s_pt+1<k) e_pt++;
+            else if(e_pt-s_pt+1==k){
+                if(cnt==0) lst.add(s_pt);
+                if(map.containsKey(s.charAt(s_pt))){
+                    map.put(s.charAt(s_pt),map.get(s.charAt(s_pt))+1);
+                    if(map.get(s.charAt(s_pt))==1){
+                        cnt++;
+                    }
+                }
+                e_pt++;
+                s_pt++;
+            }
+        }
+        return lst;
+    }
+}
+
