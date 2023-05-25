16ea1a60e85eb8fbd2e783879434f34d43a0238b
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-05-25 21:58:06 +0530

    Create Ques_1552 Magnetic Force Between Two Balls.java

---
+++ Binary_Search_--/Ques_1552 Magnetic Force Between Two Balls.java
@@ -0,0 +1,31 @@
+//In this Type of question we use advanced Binary search
+//It is similiar to aggressive cows.
+class Solution {
+    public int maxDistance(int[] pst, int m) {
+        Arrays.sort(pst);
+        int s_pt=0;
+        int e_pt=pst[pst.length-1]-pst[0];
+        int res=0;
+        while(s_pt<=e_pt){
+            int mid=s_pt+(e_pt-s_pt)/2;
+            if(isPosb(pst,mid,m)){
+                res=mid;
+                s_pt=mid+1;
+            }
+            else e_pt=mid-1;
+        }
+        return res;
+    }
+    public static boolean isPosb(int[] arr,int mid,int m){
+        int ball=1;
+        int pos=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-pos>=mid){
+                ball++;
+                pos=arr[i];
+            }  
+            if(m==ball) return true;
+        }
+        return false;
+    }
+}
