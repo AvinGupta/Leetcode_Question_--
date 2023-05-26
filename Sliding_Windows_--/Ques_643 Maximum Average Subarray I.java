0984e1b02a64d7265bafab020e974b7d0b12fd0a
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-05-26 09:46:13 +0530

    Create Ques_643 Maximum Average Subarray I.java

---
+++ Sliding_Windows_--/Ques_643 Maximum Average Subarray I.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int s_pt=0;
+        int e_pt=0;
+        int ans=Integer.MIN_VALUE;
+        int sum=0;
+        while(e_pt<nums.length){
+            sum+=nums[e_pt];
+            if(e_pt-s_pt+1<k) e_pt++;
+            else if(e_pt-s_pt+1==k){
+                ans=Math.max(ans,sum);
+                sum-=nums[s_pt];
+                e_pt++;
+                s_pt++;
+            }
+        }
+        return (ans*1.0)/k;
+    }
+}
