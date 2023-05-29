c11f68f763706e485da40b775a2305122d0069a3
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-05-29 20:33:51 +0530

    Create Ques_1343 Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.java

---
+++ Sliding_Windows_--/Ques_1343 Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int spt=0;
+        int ept=0;
+        int cnt=0;
+        int sum=0;
+        while(ept<arr.length){
+            sum+=arr[ept];
+            if(ept-spt+1<k) ept++;
+            else if(ept-spt+1==k){
+                if(sum/k>=threshold) cnt++;
+                sum-=arr[spt];
+                ept++;
+                spt++;
+            }
+        }
+        return cnt;
+    }
+}
