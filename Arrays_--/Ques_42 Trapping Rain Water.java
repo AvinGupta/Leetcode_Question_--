756fa9b978d88bd5b76d708e83bee570ae423a58
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-06-07 23:10:10 +0530

    Create Ques_42 Trapping Rain Water.java

---
+++ Arrays_--/Ques_42 Trapping Rain Water.java
@@ -0,0 +1,25 @@
+import java.lang.Math;
+class Solution {
+    public int trap(int[] nums) {
+        int[] left=new int[nums.length];
+        left[0]=nums[0];
+        int[] right=new int[nums.length];
+        right[nums.length-1]=nums[nums.length-1];
+        
+        for(int i=1;i<nums.length;i++){
+            left[i]=Math.max(left[i-1],nums[i]);
+        }
+        
+        for(int j=nums.length-2;j>=0;j--){
+            right[j]=Math.max(right[j+1],nums[j]);
+        }
+        
+        int result=0;
+        for(int i=1;i<nums.length-1;i++){
+            result+=Math.min(left[i],right[i])-nums[i];
+        }
+        
+        return result;
+    }
+    
+}
