bddc94844f8c8ecd00322b7a9a19ed78980305c7
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-06 06:42:03 +0530

    Update Ques_35 Search Insert Position.java

--- Binary_Search_--/Ques_35 Search Insert Position.java
+++ Binary_Search_--/Ques_35 Search Insert Position.java
@@ -9,4 +9,21 @@ class Solution {
         }
         return s_pt;
     }
+
+
+    //method-2 using lower bound technique
+     public int searchInsert(int[] nums, int target) {
+        int spt=0;
+        int ept=nums.length-1;
+        int ans=nums.length;
+        while(spt<=ept){
+            int mid=spt+(ept-spt)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                ept=mid-1;
+            }
+            else spt=mid+1;
+        }
+        return ans;
+    }
 }
