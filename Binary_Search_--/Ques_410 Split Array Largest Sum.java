55ab98c45b64a57e4fcf7130fead6c54c601bcd8
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-14 04:26:48 +0530

    Create Ques_410 Split Array Largest Sum.java

---
+++ Binary_Search_--/Ques_410 Split Array Largest Sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int spt=0;
+        int ept=0;
+        for(int var:nums){
+            if(var>spt) spt=var;
+            ept+=var;
+        }
+        int ans=0;
+        while(spt<=ept){
+            int mid=spt+(ept-spt)/2;
+            if(isPosb(nums,mid,k)){
+                ans=mid;
+                ept=mid-1;
+            }
+            else spt=mid+1;
+        }
+        return ans;
+    }
+    public static boolean isPosb(int[] arr,int mid,int k){
+        int var=1;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            if(sum+arr[i]<=mid){
+                sum+=arr[i];
+            }
+            else{
+                var++;
+                sum=arr[i];
+            }
+            if(var>k) return false;
+        }
+        return true;
+    }
+}
