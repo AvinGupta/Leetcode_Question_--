343943a6863b75de7bf2ad44fad1e54b4c94aa11
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-14 03:31:23 +0530

    Create Ques_1539 Kth Missing Positive Number.java

---
+++ Binary_Search_--/Ques_1539 Kth Missing Positive Number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        // brute force/naive approch:--
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]<=k) k++;
+        //     else break;
+        // }
+        // return k;
+
+        // optimised/better approch:--
+        int spt=0;
+        int ept=arr.length-1;
+        while(spt<=ept){
+            int mid=spt+(ept-spt)/2;
+            int miss=arr[mid]-(mid+1);
+            if(miss<k)  spt=mid+1;
+            else ept=mid-1;
+        }
+        return spt+k;
+    }
+}
