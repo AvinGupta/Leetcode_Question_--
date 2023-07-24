74823a7c1ff4dfc6af17cd77897b2b899f5ad001
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-25 00:48:52 +0530

    Create Ques_39 Combination Sum.java

---
+++ Recursion_and_Backtracking_--/Ques_39 Combination Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> lst=new ArrayList<>();
+        List<List<Integer>> lst_1 =new ArrayList<>();
+        Combinations(lst,lst_1,candidates,target,0);
+        return lst_1;
+    }
+    public static void Combinations(List<Integer> lst,List<List<Integer>> lst_1,int[] arr,int tar,int ind){
+        if(tar==0){
+            lst_1.add(new ArrayList<>(lst));
+            return;
+        }
+        for(int i=ind;i<arr.length;i++){
+            if(tar>=arr[i]){
+                lst.add(arr[i]);
+                Combinations(lst,lst_1,arr,tar-arr[i],i);
+                lst.remove(lst.size()-1);
+            }
+        }
+    }
+}
