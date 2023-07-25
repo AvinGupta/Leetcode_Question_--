7b2ae0fc43605185c5d4bc439cc4f2b89dc9ea0c
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-26 01:10:45 +0530

    Update Ques_77 Combinations.java

--- Recursion_and_Backtracking_--/Ques_77 Combinations.java
+++ Recursion_and_Backtracking_--/Ques_77 Combinations.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         int[] arr=new int[n];
         List<List<Integer>> lst_1=new ArrayList<>();
-        Combinations(arr,lst_1,k,0,new ArrayList<Integer>(),-1);
+        Combinations(arr,lst_1,k,0,new ArrayList<Integer>(),0);
         return lst_1;
     }
     public static void Combinations(int[] arr,List<List<Integer>> lst_1,int k,int placed,List<Integer> lst,int idx){
@@ -10,11 +10,11 @@ class Solution {
             lst_1.add(new ArrayList<>(lst));
             return;
         }
-        for(int i=idx+1;i<arr.length;i++){
+        for(int i=idx;i<arr.length;i++){
             if(arr[i]==0){
                 arr[i]=1;
                 lst.add(i+1);
-                Combinations(arr,lst_1,k,placed+1,lst,i);
+                Combinations(arr,lst_1,k,placed+1,lst,i+1);
                 lst.remove(lst.size()-1);
                 arr[i]=0;
             }
