830468f82533dc736beeddbca97a97d56e86b797
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-25 18:10:01 +0530

    Update Ques_78 Subsets.java

--- Recursion_and_Backtracking_--/Ques_78 Subsets.java
+++ Recursion_and_Backtracking_--/Ques_78 Subsets.java
@@ -17,3 +17,24 @@ class Solution {
         lst.remove(lst.size()-1);//remove value after the call
     }
 }
+
+
+// OR
+
+class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
