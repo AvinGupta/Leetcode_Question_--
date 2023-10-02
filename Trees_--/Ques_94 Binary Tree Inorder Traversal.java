a04b4aa163ddb342637edee3aa84b2f413629815
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-10-03 05:06:20 +0530

    Update Ques_94 Binary Tree Inorder Traversal.java

--- Trees_--/Ques_94 Binary Tree Inorder Traversal.java
+++ Trees_--/Ques_94 Binary Tree Inorder Traversal.java
@@ -26,3 +26,27 @@ class Solution {
         inOrder(root.right,lst);
     }
 }
+//Iterative solution using stack.
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> lst=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        if(root==null) return lst;
+        TreeNode node=root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+            else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node=st.pop();
+                lst.add(node.val);
+                node=node.right;
+            }
+        }
+        return lst;
+    }
+}
