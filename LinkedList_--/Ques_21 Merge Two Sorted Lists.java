1cb4261ffab6994b514a7ea61a2e5fe4b0a605c5
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-03-31 22:09:12 +0530

    Update Ques_21 Merge Two Sorted Lists.java

--- LinkedList_--/Ques_21 Merge Two Sorted Lists.java
+++ LinkedList_--/Ques_21 Merge Two Sorted Lists.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution { 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode ln=new ListNode();
         ListNode temp=ln;
