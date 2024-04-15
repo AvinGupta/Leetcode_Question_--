6d136aa98ed5647de8b9af406487009b1f1ac567
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-04-15 09:33:13 +0530

    Create Ques_138 Copy List with Random Pointer.java

---
+++ LinkedList_--/Ques_138 Copy List with Random Pointer.java
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public static void insertNode(Node head){
+        Node temp=head;
+        while(temp!=null){
+            Node nn=new Node(temp.val);
+            nn.next=temp.next;
+            temp.next=nn;
+            temp=temp.next.next;
+        }
+    }
+    public static void copyRandom(Node head){
+        Node temp=head;
+        while(temp!=null){
+            Node nn=temp.next;
+            if(temp.random!=null){
+                nn.random=temp.random.next;
+            }
+            else{
+                nn.random=null;
+            }
+            temp=temp.next.next;
+        }
+    }
+    public static Node seperate(Node head){
+        Node temp=head;
+        Node dummy=new Node(-1);
+        Node res=dummy;
+        while(temp!=null){
+            res.next=temp.next;
+            res=res.next;
+            temp.next=temp.next.next;
+            temp=temp.next;
+        }
+        return dummy.next;
+    }
+    public Node copyRandomList(Node head) {
+        insertNode(head);
+        copyRandom(head);
+        return seperate(head);
+    }
+}
