dc2ff95f82b2a29dd25053054aa407213b4a2edf
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-04-25 23:15:58 +0530

    Create Ques_204 Count Primes.java

--- Arrays_--/Ques_204 Count Primes.java
+++ Arrays_--/Ques_204 Count Primes.java
@@ -2,7 +2,7 @@ class Solution {
     public int countPrimes(int n) {
         if(n<2) return 0;
         int[] arr=new int[n];
-        for(int i=2;i*i<n;i++){
+        for(int i=2;i*i<n;i++){ 
             if(arr[i]==0){
                 for(int mul=2;mul*i<n;mul++){
                     arr[i*mul]=1;
