882c22901587bcea7a657c5f26ea6cc63d090924
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-05-12 23:39:06 +0530

    Create Ques_58 Length of Last Word.java

---
+++ Arrays_--/Ques Code Code Accepted Testcase Test Result Test Result 58. Length of Last Word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s.trim();
+        String[] str=s.split(" ");
+        String ans=str[str.length-1];
+        return ans.length();
+    }
+}
