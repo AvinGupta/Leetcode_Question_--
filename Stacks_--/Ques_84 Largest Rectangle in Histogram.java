269804e7d38e3669d75d743a461a5c8ed9ac6a41
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-08-08 14:48:12 +0530

    Create Ques_84 Largest Rectangle in Histogram.java

--- Stacks_--/Ques_84 Largest Rectangle in Histogram.java
+++ Stacks_--/Ques_84 Largest Rectangle in Histogram.java
@@ -1,35 +1,22 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        return Area(heights);
-    } 
-    public static int Area(int[] arr){
-        Stack<Integer> stk=new Stack<>();
-        int ans=0;
+    public int largestRectangleArea(int[] arr) {
+        Stack<Integer> st=new Stack<>();
+        int max=0;
         for(int i=0;i<arr.length;i++){
-            while(!stk.isEmpty() && arr[i]<arr[stk.peek()]){
-                int rgt=i;
-                int hgt=arr[stk.pop()];
-                if(stk.isEmpty()){
-                   ans=Math.max(ans,hgt*rgt);
-                }
-                else{
-                    int lft=stk.peek();
-                    ans=Math.max(ans,hgt*(rgt-lft-1));
-                }
+            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+                int val=st.pop();
+                int nse=i;
+                int pse=st.isEmpty()?-1:st.peek();
+                max=Math.max(max,(nse-pse-1)*arr[val]);
             }
-            stk.push(i);
+            st.push(i);
         }
-        int rgt= arr.length;
-        while(!stk.isEmpty()){
-            int hgt=arr[stk.pop()];
-            if(stk.isEmpty()){
-                ans=Math.max(ans,hgt*rgt);
-            }
-            else{
-                int lft=stk.peek();
-                ans=Math.max(ans,hgt*(rgt-lft-1));
-            }
+        while(!st.isEmpty()){
+            int val=st.pop();
+            int nse=arr.length;
+            int pse=st.isEmpty()?-1:st.peek();
+            max=Math.max(max,(nse-pse-1)*arr[val]);
         }
-        return ans;
+        return max;
     }
 }
