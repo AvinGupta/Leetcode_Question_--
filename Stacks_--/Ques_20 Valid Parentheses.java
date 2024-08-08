56c02b73a7342040d07929a889a73725c683da45
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-08-08 09:08:12 +0530

    Create Ques_20 Valid Parentheses.java

--- Stacks_--/Ques_20 Valid Parentheses.java
+++ Stacks_--/Ques_20 Valid Parentheses.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isValid(String str) {
-      if(str.length()%2!=0) return false;
-			Stack<Character> stck=new Stack<>();
-			for(char chr:str.toCharArray()){
-			if(chr=='(' || chr=='[' || chr=='{') stck.push(chr);
-			else if(chr==')' && !stck.isEmpty() && stck.peek()=='(') stck.pop();
-			else if(chr==']' && !stck.isEmpty() && stck.peek()=='[') stck.pop();
-			else if(chr=='}' && !stck.isEmpty() && stck.peek()=='{') stck.pop();
-            else return false;
-		}
-		return stck.isEmpty();
+        if(str.length()%2!=0) return false;
+	Stack<Character> stck=new Stack<>();
+	for(char chr:str.toCharArray()){
+		if(chr=='(' || chr=='[' || chr=='{') stck.push(chr);
+		else if(chr==')' && !stck.isEmpty() && stck.peek()=='(') stck.pop();
+		else if(chr==']' && !stck.isEmpty() && stck.peek()=='[') stck.pop();
+		else if(chr=='}' && !stck.isEmpty() && stck.peek()=='{') stck.pop();
+            	else return false;
+	}
+	return stck.isEmpty();
     }
 }
