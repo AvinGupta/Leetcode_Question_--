a1526e0a92098472473300a003895ff3e2518c6f
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-08-08 16:11:22 +0530

    Create Ques_907 Sum of Subarray Minimums.java

---
+++ Stacks_--/Ques_907 Sum of Subarray Minimums.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] nextGreaterElement(int[] arr){
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()) ans[i]=st.peek();
+            else ans[i]=arr.length;
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] previousSmallerElement(int[] arr){
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            while(!st.isEmpty() && arr[st.peek()]>arr[i]){// here we are removing only greater element not the equal ones.
+              //because we dont want to consider duplicate element twice for ex.{1,1} array were nse{2,2} and pse{-1,0}.
+                st.pop();
+            }
+            if(!st.isEmpty()) ans[i]=st.peek();
+            else ans[i]=-1;
+            st.push(i);
+        }
+        return ans;
+    }
+    public int sumSubarrayMins(int[] arr) {
+        int[] nge=nextGreaterElement(arr);
+        int[] pse=previousSmallerElement(arr);
+        long ans=0;
+        int mod=(int)1e9+7;
+        for(int i=0;i<arr.length;i++){
+            int left=i-pse[i];
+            int right=nge[i]-i;
+            ans=(ans+(long)right*left*arr[i])%mod;
+        }
+        return (int)ans;
+    }
+}
